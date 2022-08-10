3f197619c33b744ff36d20befdc1289a4d3044c9
unknown <maik_mamola@hotmail.com>
2022-08-10 11:46:37 -0300

    primeiro commit

---
+++ Contagemdepeças.js
@@ -0,0 +1,13 @@
+//contagem de peças
+let contador = 60;
+
+let peças = contador;
+
+if(peças >= 100){//true
+
+console.log("QUANTIDADE MAIOR OU IGUAL: Tudo Certo Obrigado");
+}else { //false
+
+
+console.log("QUANTIDADE MENOR: Quantidade Incorreta");
+}
