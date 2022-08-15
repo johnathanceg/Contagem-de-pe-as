3ac59184b4d1c6a2303a7a66f63b260af2097a7b
johnathanceg <100812728+johnathanceg@users.noreply.github.com>
2022-08-15 19:54:22 -0300

    Add files via upload

---
+++ cadastro-pecas4.js
@@ -0,0 +1,28 @@
+//Cadastro de Peças
+const entrada = require("readline-sync");
+
+let listaPecas = 11;
+let peso = 80;
+let nomePeca = "Pistão do Motor"; //string
+let nome = entrada.question("Qual o nome da Peca?");
+
+if(nome = "Pistao do Motor"); {
+    console.log("Obrigado");
+
+} 
+
+if(peso >= 100 ){
+    console.log("Peça tem peso permitido para cadastrar");
+} else {
+    console.log("Peça muito leve");
+}
+
+if(listaPecas >= 10){
+    console.log("O numero de Usuarios ja esta no limite");
+}
+
+if(nomePeca.length < 3) {
+    console.log("Nome muito Curto!!");
+}
+
+console.log("Fim do Programa");
